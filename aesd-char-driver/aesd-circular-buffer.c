7656b2416565a43ad336febecdca16ee990f75f5
Steven Turgetto <steven.turgetto@gmail.com>
2026-02-05 09:19:14 -0800

    Assignment 7

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -32,7 +32,41 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
-    return NULL;
+    size_t curr_coff = char_offset;
+    size_t curr_boff = buffer->out_offs;
+
+    struct aesd_buffer_entry * retval = NULL;
+    while (true)
+    {
+	// if the size of the current element is greater than the current
+	// character offset...
+        if (buffer->entry[curr_boff].size <= curr_coff)
+	{
+	    // adjust the current character offset
+            curr_coff -= buffer->entry[curr_boff].size;
+	    // move to the next buffer offset.
+            if (curr_boff == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED-1)
+                curr_boff = 0;
+	    else
+	        curr_boff++;
+	    // If the current buffer osset equals the input offset, there
+	    // isn't an element at this offset.
+	    if (curr_boff == buffer->in_offs)
+	    {
+		*entry_offset_byte_rtn = 0;
+		retval = NULL;
+		break;
+	    }
+	}
+	// Else, the offset is in the current buffer offset, return it.
+	else
+        {
+            *entry_offset_byte_rtn = curr_coff;
+	    retval = &buffer->entry[curr_boff];
+            break;
+        }
+    }
+    return retval;
 }
 
 /**
@@ -47,6 +81,30 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+    // if the circular buffer is true, increment the out pointer.
+    if (buffer->full == true)
+    {
+        if (buffer->out_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED-1)
+            buffer->out_offs = 0;
+        else
+            buffer->out_offs++;
+    }
+
+    // Copy the incoming buffer entry over.
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+    buffer->entry[buffer->in_offs].size    = add_entry->size;
+
+    // Increment the in pointer.
+    if (buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED-1)
+        buffer->in_offs = 0;
+    else
+        buffer->in_offs++;
+
+    // Is the in and out point at each other.
+    if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }
 }
 
 /**
